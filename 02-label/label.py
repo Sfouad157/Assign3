66c0eca083f2cc063bfabe3e4f0c7834c7ffd630
Sfouad157 <sfouad@augusta.edu>
2025-05-30 07:48:49 -0400

    Finished assignment

--- 02-label/label.py
+++ 02-label/label.py
@@ -1,14 +1,18 @@
 # Prompt the user to for a first name
+first_name = input("Enter a first name: ")
 
 
 # Prompt the user to for a last name
+last_name = input("Enter a last name: ")
 
 
 # Prompt the user for a job title
-
+job_title = input("Enter a job title: ")
 
 # Print the values label in the form:
 #
 # last_name, first_name
-# title
 
+# Print output in specified format
+print(f"{last_name}, {first_name}")
+print(job_title)
--- 04-tip/tip.py
+++ 04-tip/tip.py
@@ -1,16 +1,16 @@
 # Get input for the check amount.
 # Convert to float.
-
+meal_cost = float(input("Enter meal cost: "))
 
 # Get input for the tip percent. Convert to int.
-
+tip_percent = int(input("Enter tip percent: "))
 
 # Calculate the tip amount.  Don't forget to divide by 100 to a *real* percent.
-
+tip_amount = meal_cost * (tip_percent / 100)
 
 # Print the tip results in the format `A PCT% tip on $CHECK is $AMOUNT`. Except
 # for the tip PCT (which should be an int), just let the number of decimal
 # places run long or short. We'll learn how to round and format these numbers
 # later. You may want to construct an output string a step at a time to avoice a
 # really long concatenation.
-
+print(f"A {tip_percent}% tip on ${meal_cost} is ${tip_amount}")
