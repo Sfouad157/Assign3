6e16704970cdc6958abaa83fd7863aa01dd593af
Sfouad157 <sfouad@augusta.edu>
2025-05-30 16:18:37 -0400

    Finished Assignment SFOUAD

--- 03-einstein/einstein.py
+++ 03-einstein/einstein.py
@@ -1,9 +1,10 @@
 # Prompt the user for mass as an integer (in kilograms)
-
-
+mass = int(input("Enter mass in kilograms: "))
+c = 300000000
 # Calculate the Einstein formula (don't forget to convert the input from str to
 # int)
 
-
+energy = mass * c * c
 # Print the result
 
+print(f"E: {energy}")
